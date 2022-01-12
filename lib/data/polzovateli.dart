a22777bdf6543de97a930138865aa398efcf9456
VseSchev <trendaila1976@gmail.com>
2022-01-12 11:44:27 +0300

    сделано

---
+++ lib/data/polzovateli.dart
@@ -0,0 +1,7 @@
+import '../model/polzovatel.dart';
+
+List<User> Users_list = [
+  User(name: 'Котелков Петр', phone: '8-999-237-22-34'),
+  User(name: 'Абдурахманов Халиль', phone: '8-495-763-83-76'),
+  User(name: 'Михеева Зинаида', phone: '8-10-7672-87348-34543'),
+];
